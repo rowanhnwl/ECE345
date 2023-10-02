94f20ad4aad87d63fa496ea54ff8c3a1a4d8eb1e
rowanhnwl <rowan.honeywell@mail.utoronto.ca>
2023-10-02 00:25:20 -0400

    Added a 'movezeros' solution

--- miscellaneous/movezeros/movezeros.c
+++ miscellaneous/movezeros/movezeros.c
@@ -3,20 +3,49 @@
 
 void swap(int*, int, int);
 
-int printarr(int*, size_t);
-int movezeros(int*, size_t);
+void printarr(int*, size_t);
+void movezeros_back(int*, size_t);
+void movezeros_front(int*, size_t);
 
 int main(void){
     int arr[] = {5, 0, 3, 1, 0, 5, 6, 2, 0, 0, 1, 7};
+    size_t size = sizeof(arr) / sizeof(arr[0]);
+
+    movezeros_front(arr, size);
+    printarr(arr, size);
 }
 
-int printarr(int* a, size_t s){
-    for (int i = 0; i < s; i++){
+void swap(int* a, int u, int v){
+    int t = a[u];
+    a[u] = a[v];
+    a[v] = t;
+}
+
+void printarr(int* a, size_t s){
+    for (int i = 0; i < (int)s; i++){
         printf("%d ", a[i]);
     }
     printf("\n");
 }
 
-int movezeros(int* a, size_t s){
+void movezeros_back(int* a, size_t s){
+    int i = -1;
 
+    for (int j = 0; j < (int)s; j++){
+        if (a[j] > 0){
+            i++;
+            swap(a, i, j);
+        }
+    }
+}
+
+void movezeros_front(int* a, size_t s){
+    int i = s;
+
+    for (int j = (int)(s - 1); j >= 0; j--){
+        if (a[j] > 0){
+            i--;
+            swap(a, i, j);
+        }
+    }
 }
