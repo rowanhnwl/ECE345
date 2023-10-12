edb0ae88de9185d8cde5ea1144b1fa99f80faf36
Ardavan Alaei Fard <ardavanalaei@gmail.com>
2023-10-12 02:05:46 -0400

    Optimized the algorithms

--- hw/test/HW2/q3/HW2Q3.cpp
+++ hw/test/HW2/q3/HW2Q3.cpp
@@ -11,33 +11,47 @@ using namespace std;
 void find_union (int size, Lists* all_lists) {
     int a_ptr = 0;
     int b_ptr = 0;
+    int most_recent = -1;
 
     while (a_ptr < size && b_ptr < size) {
         if (all_lists->a_vec[a_ptr] == all_lists->b_vec[b_ptr]) {
-            all_lists->union_list.push_back(all_lists->a_vec[a_ptr]);
+            if (all_lists->a_vec[a_ptr] > most_recent) {
+                all_lists->union_list.push_back(all_lists->a_vec[a_ptr]);
+                most_recent = all_lists->a_vec[a_ptr];
+            }
             a_ptr++;
             b_ptr++;
         }
         else if (all_lists->a_vec[a_ptr] < all_lists->b_vec[b_ptr]) {
-            all_lists->union_list.push_back(all_lists->a_vec[a_ptr]);
+            if (all_lists->a_vec[a_ptr] > most_recent) {
+                all_lists->union_list.push_back(all_lists->a_vec[a_ptr]);
+                most_recent = all_lists->a_vec[a_ptr];
+            }
             a_ptr++;
         }
         else if (all_lists->a_vec[a_ptr] > all_lists->b_vec[b_ptr]) {
-            all_lists->union_list.push_back(all_lists->b_vec[b_ptr]);
+            if (all_lists->b_vec[b_ptr] > most_recent) {
+                all_lists->union_list.push_back(all_lists->b_vec[b_ptr]);
+                most_recent = all_lists->b_vec[b_ptr];
+            }
             b_ptr++;
         }
     }
 
     if (a_ptr == size) {
             for (;b_ptr < size; b_ptr++)
-                if (all_lists->a_vec[a_ptr - 1] < all_lists->b_vec[b_ptr])
+                if (all_lists->a_vec[size - 1] < all_lists->b_vec[b_ptr] && all_lists->b_vec[b_ptr] > most_recent) {
                     all_lists->union_list.push_back(all_lists->b_vec[b_ptr]);
+                    most_recent = all_lists->b_vec[b_ptr];
+                }
         }
 
         if (b_ptr == size) {
             for (;a_ptr < size; a_ptr++)
-                if (all_lists->a_vec[a_ptr] > all_lists->b_vec[b_ptr - 1])
+                if (all_lists->a_vec[a_ptr] > all_lists->b_vec[size - 1] && all_lists->a_vec[a_ptr] > most_recent) {
                     all_lists->union_list.push_back(all_lists->a_vec[a_ptr]);
+                    most_recent = all_lists->a_vec[a_ptr];
+                }
         }
 
     for (int i : all_lists->union_list) {
@@ -49,10 +63,14 @@ void find_union (int size, Lists* all_lists) {
 void find_intersection (int size, Lists* all_lists) {
     int a_ptr = 0;
     int b_ptr = 0;
+    int most_recent = -1;
 
     while (a_ptr < size && b_ptr < size) {
         if (all_lists->a_vec[a_ptr] == all_lists->b_vec[b_ptr]) {
-            all_lists->intersection_list.push_back(all_lists->a_vec[a_ptr]);
+            if (all_lists->a_vec[a_ptr] != most_recent) {
+                all_lists->intersection_list.push_back(all_lists->a_vec[a_ptr]);
+                most_recent = all_lists->a_vec[a_ptr];
+            }
             a_ptr++;
             b_ptr++;
         }
@@ -66,14 +84,18 @@ void find_intersection (int size, Lists* all_lists) {
 
     if (a_ptr == size) {
         for (;b_ptr < size; b_ptr++)
-            if (all_lists->a_vec[a_ptr] > all_lists->a_vec[a_ptr - 1] && all_lists->a_vec[a_ptr - 1] == all_lists->b_vec[b_ptr])
+            if (all_lists->a_vec[size - 1] == all_lists->b_vec[b_ptr] && all_lists->b_vec[b_ptr] != most_recent) {
                 all_lists->intersection_list.push_back(all_lists->b_vec[b_ptr]);
+                most_recent = all_lists->b_vec[b_ptr];
+            }
     }
 
     if (b_ptr == size) {
         for (;a_ptr < size; a_ptr++)
-            if (all_lists->a_vec[a_ptr] > all_lists->a_vec[a_ptr - 1] && all_lists->a_vec[a_ptr] == all_lists->b_vec[b_ptr - 1])
+            if (all_lists->a_vec[a_ptr] == all_lists->b_vec[size - 1] && all_lists->a_vec[a_ptr] != most_recent) {
                 all_lists->intersection_list.push_back(all_lists->a_vec[a_ptr]);
+                most_recent = all_lists->a_vec[a_ptr];
+            }
     }
 
     for (int i : all_lists->intersection_list) {
@@ -84,23 +106,22 @@ void find_intersection (int size, Lists* all_lists) {
 
 void find_diff (int size, Lists* all_lists, vector<int>* include, string diff_list) {
     int i_ptr = 0;
+    int most_recent = -1;
     list<int>::iterator it = all_lists->intersection_list.begin();
 
     while (i_ptr < size) {
-        if (it == all_lists->intersection_list.end()) {
-            (all_lists->diff)[diff_list].push_back(include->at(i_ptr));
-            i_ptr++;
-        }
-        else if (include->at(i_ptr) == *it) {
-            i_ptr++;
-            it++;
-        }
-        else if (include->at(i_ptr) < *it) {
-            (all_lists->diff)[diff_list].push_back(include->at(i_ptr));
+        if (include->at(i_ptr) < *it || it == all_lists->intersection_list.end()) {
+            if (include->at(i_ptr) != most_recent) {
+                (all_lists->diff)[diff_list].push_back(include->at(i_ptr));
+                most_recent = include->at(i_ptr);
+            }
             i_ptr++;
         }
         else if (include->at(i_ptr) > *it) {
             it++;
+        }
+        else {
+            i_ptr++;
         }
     }
 
@@ -111,8 +132,8 @@ void find_diff (int size, Lists* all_lists, vector<int>* include, string diff_li
 }
 
 int main() {
-    vector<int> array_a = {1, 2, 5, 6, 10, 11, 12, 23};
-    vector<int> array_b = {2, 5, 5, 5, 23, 23, 23, 25};
+    vector<int> array_a = {1, 2, 5, 6, 10, 10, 22, 31, 43};
+    vector<int> array_b = {2, 2, 7, 8, 10, 15, 17, 22, 31};
     int size = array_a.size();
 
     Lists* lists = new Lists;
