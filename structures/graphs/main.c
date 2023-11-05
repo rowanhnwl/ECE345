9033b0912197358ef55b68fcee9182dfcb5a718d
rowanhnwl <rowan.honeywell@mail.utoronto.ca>
2023-11-04 22:41:50 -0400

    Added initial code for graph algorithms

---
+++ structures/graphs/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "structures/adjlist/list.h"
+
+int main(void){
+    NodeList* list = init_list();
+
+    list_insert(list, 4);
+    list_insert(list, 2);
+    list_insert(list, 1);
+    list_insert(list, 6);
+    list_insert(list, 7);
+    list_insert(list, 5);
+    list_insert(list, 3);
+    list_insert(list, 8);
+
+    print_list(list);
+
+    list_delete(list, 4);
+    list_delete(list, 8);
+    list_delete(list, 7);
+
+    print_list(list);
+
+    if (list_search(list, 5) != NULL){
+        printf("Found 5\n");
+    }
+
+    list_dealloc(list);
+}
